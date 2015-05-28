78644c3bc74c59f3e0c5133a5d814381c6e1249c
tonic <tonicbupt@gmail.com>
2015-05-28 16:19:09 +0800

    new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='erucli',
-    version='0.1.5',
+    version='0.1.6',
     author='tonic',
     zip_safe=False,
     author_email='tonic@wolege.ca',
