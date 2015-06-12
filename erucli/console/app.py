d3054e55f9459d3057e9813e03a9fa538457334e
tonic <tonicbupt@gmail.com>
2015-06-12 14:40:32 +0800

    add specific ips

--- erucli/console/app.py
+++ erucli/console/app.py
@@ -111,11 +111,12 @@ def alloc_resource(ctx, env, res_name, name):
 @click.option('--version', '-v', default=None, help='version to deploy')
 @click.option('--network', '-i', help='version to deploy', multiple=True)
 @click.option('--host', '-h', help='specific host name', default=None, type=str)
+@click.option('--ip', '-p', help='specific ip', multiple=True)
 @click.option('--raw', '-r', help='deploy a raw image', is_flag=True)
 @click.option('--image', '-m', help='specific image', default='', type=str)
 @click.pass_context
 def deploy_private_container(ctx, group, pod, entrypoint,
-        env, ncore, ncontainer, version, network, host, raw, image):
+        env, ncore, ncontainer, version, network, host, ip, raw, image):
     eru = ctx.obj['eru']
 
     network_ids = []
@@ -130,7 +131,7 @@ def deploy_private_container(ctx, group, pod, entrypoint,
         version = ctx.obj['short_sha1']
     r = eru.deploy_private(group, pod, ctx.obj['appname'], ncore,
             ncontainer, version, entrypoint, env, network_ids,
-            host, raw, image)
+            host, raw, image, ip)
 
     if r['r']:
         click.echo(error(r['msg']))
@@ -169,11 +170,12 @@ def deploy_private_container(ctx, group, pod, entrypoint,
 @click.option('--ncontainer', '-n', default=1, help='how many containers', type=int)
 @click.option('--version', '-v', default=None, help='version to deploy')
 @click.option('--network', '-i', help='version to deploy', multiple=True)
+@click.option('--ip', '-p', help='specific ip', multiple=True)
 @click.option('--raw', '-r', help='deploy a raw image', is_flag=True)
 @click.option('--image', '-m', help='specific image', default='', type=str)
 @click.pass_context
 def deploy_public_container(ctx, group, pod, entrypoint, env, ncontainer,
-        version, network, raw, image):
+        version, network, ip, raw, image):
     eru = ctx.obj['eru']
 
     network_ids = []
@@ -188,7 +190,7 @@ def deploy_public_container(ctx, group, pod, entrypoint, env, ncontainer,
         version = ctx.obj['short_sha1']
 
     r = eru.deploy_public(group, pod, ctx.obj['appname'],
-            ncontainer, version, entrypoint, env, network_ids, raw, image)
+            ncontainer, version, entrypoint, env, network_ids, raw, image, ip)
 
     if r['r']:
         click.echo(error(r['msg']))
